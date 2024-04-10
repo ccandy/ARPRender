25dc55af094a80a4750dd6ad665f31a717f4dc9b
ccandy <ccunandy2001@gmail.com>
2024-04-10 11:33:56 -0700

    add command buffer setup execute buffer functions to camera render

--- Assets/Scripts/Runtime/Renderpipleline/CameraRender.cs
+++ Assets/Scripts/Runtime/Renderpipleline/CameraRender.cs
@@ -5,6 +5,8 @@ using UnityEngine.Rendering;
 
 public class CameraRender
 {
+    private const string bufferName = "Render Camera";
+    private CommandBuffer _cameraBuffer;
     private ScriptableRenderContext context;
     private Camera _camera;
 
@@ -12,7 +14,47 @@ public class CameraRender
     {
         this.context = context;
         this._camera = camera;
+        
+        Setup();
+        ExecuteAndClearBuffer();
+        DrawVisibleGeometry();
+        Submit();
     }
 
+    void Setup()
+    {
+        //setup camera matrix
+        context.SetupCameraProperties(_camera);
+        
+        //setup command buffer
+        if (_cameraBuffer == null)
+        {
+            _cameraBuffer = new CommandBuffer
+            {
+                name = bufferName
+            };
+        }
+        _cameraBuffer.ClearRenderTarget(true, true, Color.clear);
+        _cameraBuffer.BeginSample(bufferName);
+    }
+    
+    void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(_camera);
+    }
+
+    void Submit()
+    {
+        _cameraBuffer.EndSample(bufferName);
+        context.Submit();
+    }
+
+    void ExecuteAndClearBuffer()
+    {
+        context.ExecuteCommandBuffer(_cameraBuffer);
+        _cameraBuffer.Clear();
+    }
+    
+    
 
 }
