8d3232c41216151977d9f9d6a9f1f331d2cca1aa
ccandy <ccunandy2001@gmail.com>
2024-08-14 09:40:00 -0700

    Add function calculate specular

--- Assets/Scripts/Runtime/ShaderLib/Lighting.hlsl
+++ Assets/Scripts/Runtime/ShaderLib/Lighting.hlsl
@@ -20,4 +20,23 @@ float3 GetLighting(Surface surface)
     return lightCol;
 }
 
+float SpecularStrength(Surface surface, BRDF brdf, Light light)
+{
+    float roughness = brdf.roughness;
+    float3 lightDir = light.lightDir;
+    float3 viewDir = surface.viewDir;
+    float3 normal = surface.normal;    
+
+    
+    float3 h = SafeNormalize(lightDir + viewDir);
+    float nh2 = Square(saturate(dot(normal, h)));
+    float lh2 = Square(saturate(dot(lightDir, h)));
+    float r2 = Square(roughness);
+    float d2 = Square(nh2 * (r2 - 1) + 1.0001);
+    float normalization = roughness * 4.0 + 2.0;
+    float result = r2 / (d2 * max(0.1, lh2) * normalization);
+    
+}
+
+
 #endif
