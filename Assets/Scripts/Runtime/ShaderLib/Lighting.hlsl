9b8caf9b712725427f1442dbc868e885fc678435
ccandy <ccunandy2001@gmail.com>
2024-08-13 19:30:58 -0700

    Remove unused function

--- Assets/Scripts/Runtime/ShaderLib/Lighting.hlsl
+++ Assets/Scripts/Runtime/ShaderLib/Lighting.hlsl
@@ -6,10 +6,5 @@ float3 GetIncomingLight(Surface surface, Light light)
     return saturate(dot(surface.normal, light.lightDir)) * light.color;
 }
 
-float3 GetLighting(Surface surface, Light light)
-{
-    return GetIncomingLight(surface, light) * surface.color;
-}
-
 
 #endif
