377fab3060064d211cb9c7eb4d67b74f96c52506
ccandy <ccunandy2001@gmail.com>
2024-08-14 08:49:53 -0700

    create a function GetSurface

--- Assets/Scripts/Runtime/ShaderLib/Surface.hlsl
+++ Assets/Scripts/Runtime/ShaderLib/Surface.hlsl
@@ -5,9 +5,24 @@ struct Surface
 {
     float3 normal;
     float3 color;
+    float3 viewDir;
     float metallic;
     float roughness;
     float alpha;
 };
+
+Surface GetSurface(float4 col, float3 normal, float roughness, float metallic)
+{
+    Surface surface;
+
+    surface.color = col.rgb;
+    surface.alpha = col.a;
+    surface.normal = normal;
+    surface.roughness = roughness;
+    surface.metallic = metallic;
+
+    return surface;
+}
+
 
 #endif
