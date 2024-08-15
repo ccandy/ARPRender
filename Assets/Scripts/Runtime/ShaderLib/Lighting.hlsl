46b3eb1292a765e239dbd6fd51a94d9486c0de44
ccandy <ccunandy2001@gmail.com>
2024-08-14 20:54:49 -0700

    add missing return

--- Assets/Scripts/Runtime/ShaderLib/Lighting.hlsl
+++ Assets/Scripts/Runtime/ShaderLib/Lighting.hlsl
@@ -6,20 +6,6 @@ float3 GetIncomingLight(Surface surface, Light light)
     return saturate(dot(surface.normal, light.lightDir)) * light.color;
 }
 
-
-float3 GetLighting(Surface surface)
-{
-    float3 lightCol = 0;
-    
-    for(int n = 0; n < _direcionalLightCount; n++)
-    {
-        Light light = GetDirectionLight(n);
-        lightCol += GetIncomingLight(surface, light);
-    }
-
-    return lightCol;
-}
-
 float SpecularStrength(Surface surface, BRDF brdf, Light light)
 {
     float roughness = brdf.roughness;
@@ -35,8 +21,28 @@ float SpecularStrength(Surface surface, BRDF brdf, Light light)
     float d2 = Square(nh2 * (r2 - 1) + 1.0001);
     float normalization = roughness * 4.0 + 2.0;
     float result = r2 / (d2 * max(0.1, lh2) * normalization);
+
+    return result;
     
 }
 
+
+float3 DirectBRDF(Surface surface, BRDF brdf, Light light)
+{
+    return SpecularStrength(surface, brdf, light);
+}
+
+float3 GetLighting(Surface surface, BRDF brdf)
+{
+    float3 lightCol = 0;
+    
+    for(int n = 0; n < _direcionalLightCount; n++)
+    {
+        Light light = GetDirectionLight(n);
+        lightCol += GetIncomingLight(surface, light) * DirectBRDF(surface, brdf, light);
+    }
+
+    return lightCol;
+}
 
 #endif
