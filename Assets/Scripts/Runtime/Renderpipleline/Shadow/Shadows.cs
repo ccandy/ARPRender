628755b6cfc0ce37c3de61b4a3ee73f32123cf46
ccandy <ccunandy2001@gmail.com>
2024-08-15 21:25:36 -0700

    Add shadow altasid refractor code

--- Assets/Scripts/Runtime/Renderpipleline/Shadow/Shadows.cs
+++ Assets/Scripts/Runtime/Renderpipleline/Shadow/Shadows.cs
@@ -6,9 +6,13 @@ using UnityEngine.Rendering;
 
 public class Shadows
 {
+    
+    //Shader PropertyID
+    private static int dirShadowAtlasId = Shader.PropertyToID("_DirectionalShadowAtlas");
+    
     private const string bufferName = "Shadow Buffer";
     private const int MaxDirectionalShadow = 1;
-
+    
     private CommandBuffer _shadowBuffer = new CommandBuffer()
     {
         name = bufferName
@@ -37,6 +41,30 @@ public class Shadows
 
         _directionalShadowCount = 0;
     }
+
+    public void Render()
+    {
+        if (_directionalShadowCount > 0)
+        {
+            RenderDirectionalShadow();
+        }
+        else
+        {
+            _shadowBuffer.GetTemporaryRT(dirShadowAtlasId, 1,1, 24,FilterMode.Bilinear,RenderTextureFormat.Shadowmap);
+        }
+    }
+
+    private void RenderDirectionalShadow()
+    {
+        int altasSize = (int)_shadowSettings.DirecionalShadowSetting.AltasSize;
+        _shadowBuffer.GetTemporaryRT(dirShadowAtlasId, altasSize,altasSize, 24, FilterMode.Bilinear, RenderTextureFormat.Shadowmap);
+        _shadowBuffer.SetRenderTarget(dirShadowAtlasId, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store);
+        _shadowBuffer.ClearRenderTarget(true, false, Color.clear);
+        _context.ExecuteCommandBuffer(_shadowBuffer);
+        _shadowBuffer.Clear();
+        
+    }
+    
     
     void ExecuteBuffer()
     {
@@ -57,4 +85,14 @@ public class Shadows
 
         _directionalShadows[_directionalShadowCount++] = directionalShadow;
     }
+
+    public void CleanUp()
+    {
+        _shadowBuffer.ReleaseTemporaryRT(dirShadowAtlasId);
+        _context.ExecuteCommandBuffer(_shadowBuffer);
+        _shadowBuffer.Clear();
+    }
+    
+    
+    
 }
