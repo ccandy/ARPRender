37e1ed768bb303af75e920c60b5bcdb12f8cb9d5
ccandy <ccunandy2001@gmail.com>
2024-08-26 14:26:06 -0700

    Add GI marco

--- Assets/Scripts/Runtime/ShaderLib/Light/GI.hlsl
+++ Assets/Scripts/Runtime/ShaderLib/Light/GI.hlsl
@@ -4,7 +4,20 @@
 TEXTURE2D(unity_Lightmap);
 SAMPLER(samplerunity_Lightmap);
 
+#if defined(LIGHTMAP_ON)
 
+    #define GI_VERTEXINPUT_DATA float2 lightMapUV:TEXCOORD1;
+    #define GI_VERTEXOUTPUT_DATA float lightMapUV: VAR_LIGHT_MAP_UV;
+    #defineTRANSFORM_GI_DATA(input,output)
+        output.lightMapUV = input.lightMapUV
+    #define GI_FRAG_DATA(input) input.lightMapUV
+
+#else
+    #define GI_VERTEXINPUT_DATA 
+    #define GI_VERTEXOUTPUT_DATA 
+    #define TRANSFORM_GI_DATA(input,output)
+    #define GI_FRAG_DATA(input) 0.0
+#endif
 
 struct GI
 {
