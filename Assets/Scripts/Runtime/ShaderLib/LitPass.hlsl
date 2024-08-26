f2d94750ce4a6e6aa196361d3442dfa5da398be2
ccandy <ccunandy2001@gmail.com>
2024-08-25 23:32:22 -0700

    update GI and lightmap

--- Assets/Scripts/Runtime/ShaderLib/LitPass.hlsl
+++ Assets/Scripts/Runtime/ShaderLib/LitPass.hlsl
@@ -11,6 +11,11 @@ struct VertexInput
     float3 positionOS: POSITION;
     float2 uv: TEXCOORD0;
     float3 normal:NORMAL;
+    float2 uv2:TEXCOORD1;
+#if defined(LIGHTMAP_ON)
+    
+#endif
+    
 };
 
 struct VertexOutput
@@ -19,6 +24,10 @@ struct VertexOutput
     float3 positionWS:VAR_POSITION;
     float2 uv:VAR_BASE_UV;
     float3 normalWS:VAR_NORMAL;
+    float2 lightmapUV:TEXCOORD0;
+#if defined(LIGHTMAP_ON)
+    
+#endif
 };
 
 VertexOutput LitPassVertex(VertexInput input)
@@ -28,6 +37,9 @@ VertexOutput LitPassVertex(VertexInput input)
     output.positionCS = TransformObjectToHClip(input.positionOS);
     output.uv = input.uv * _MainTex_ST.xy + _MainTex_ST.zw;
     output.normalWS =  TransformObjectToWorldNormal(input.normal);
+    
+    //output.lightmapUV = unity_LightmapST.xy + unity_LightmapST.zw;
+
     
     return output;
 }
@@ -45,8 +57,8 @@ half4 LitPassFrag(VertexOutput input) : SV_TARGET
     Surface surface = GetSurface(col, input.normalWS, input.positionWS, _Roughness, _Metallic);
     
     BRDF brdf = GetBRDF(surface);
-    
-    float3 lightColor = GetLighting(surface, brdf);
+    GI gi = GetGI(0);
+    float3 lightColor = GetLighting(surface, brdf, gi);
 
     float3 finalCol = lightColor * brdf.diffuse;
     
