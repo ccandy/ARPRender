0960f8d51c6fa38e37c44859a8f37af4d950aa55
ccandy <ccunandy2001@gmail.com>
2024-08-27 19:22:06 -0700

    setup point light

--- Assets/Scripts/Runtime/Renderpipleline/Lighting.cs
+++ Assets/Scripts/Runtime/Renderpipleline/Lighting.cs
@@ -7,21 +7,30 @@ using UnityEngine.UI;
 
 public class Lighting
 {
+    //directional light
     private static int
         dirLightColorId = Shader.PropertyToID("_directionalLightColors"),
         dirLightDirId = Shader.PropertyToID("_directionalLightDirs"),
         dirLightCountId = Shader.PropertyToID("_direcionalLightCount");
-    
-    
-    private const string bufferName = "CustomLight";
     private const int max_dirLight_count = 4;
-    
     private int dirLightCount = 0;
     private Vector4[] dirLightColors = new Vector4[max_dirLight_count];
     private Vector4[] dirLightDirs = new Vector4[max_dirLight_count];
+    
+    //additional light
+    private static int
+        additionalLightCountId = Shader.PropertyToID("_additionalLightCount"),
+        additionalLightColorsId = Shader.PropertyToID("_additionalLightColors"),
+        additionalLightPosId = Shader.PropertyToID("additionalLightPos");
+    private const int max_additionallight_count = 64;
+    private int additonalLightCount = 0;
+    private static Vector4[]
+        additionalLightColors = new Vector4[max_additionallight_count],
+        additionalPositions = new Vector4[max_additionallight_count];
+    
+    //buffers
+    private const string bufferName = "CustomLight";
     private Shadows _shadows;
-    
-    
     private ShadowSettings _shadowSettings;
     
     private CommandBuffer cmd = new CommandBuffer{
@@ -40,7 +49,7 @@ public class Lighting
         }
         _shadows.Setup(context, cullingResults,_shadowSettings);
 
-        SetupLights(visibleLights);
+        SetupLights(context, visibleLights);
         
         _shadows.Render();
         
@@ -49,7 +58,7 @@ public class Lighting
         cmd.Clear();
     }
 
-    private void SetupLights(NativeArray<VisibleLight> visibleLights)
+    private void SetupLights(ScriptableRenderContext context, NativeArray<VisibleLight> visibleLights)
     {
         for (int n = 0; n < visibleLights.Length; n++)
         {
@@ -58,13 +67,39 @@ public class Lighting
             if (visibleLight.lightType == LightType.Directional)
             {
                 SetupDirectionalLight(visibleLight, n);
+            }else if (visibleLight.lightType == LightType.Spot || visibleLight.lightType == LightType.Point)
+            {
+                SetupAdditionalLight(visibleLight, n);
             }
         }
+
+        SetDirLightToGPU();
+        SetAdditionalLightToGPU();
+        cmd.EndSample(bufferName);
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+
+    }
+
+    private void SetupAdditionalLight(VisibleLight visibleLight, int visibleLightIndex)
+    {
+        if (additonalLightCount >= max_additionallight_count)
+        {
+            return;
+        }
+
+        additionalLightColors[additonalLightCount] = visibleLight.finalColor;
+        if (visibleLight.lightType == LightType.Point)
+        {
+            SetupPointLight(additonalLightCount, ref visibleLight);
+        }
         
-        
-        cmd.SetGlobalVectorArray(dirLightColorId, dirLightColors);
-        cmd.SetGlobalVectorArray(dirLightDirId, dirLightDirs);
-        cmd.SetGlobalFloat(dirLightCountId, dirLightCount);
+        additonalLightCount++;
+    }
+
+    private void SetupPointLight(int index, ref VisibleLight visibleLight)
+    {
+        additionalPositions[index] = visibleLight.localToWorldMatrix.GetColumn(3);
     }
     
     private void SetupDirectionalLight(VisibleLight visibleLight, int visualLightIndex)
@@ -90,6 +125,26 @@ public class Lighting
         }
         
         _shadows.CleanUp();
+    }
+
+    private void SetDirLightToGPU()
+    {
+        if (dirLightCount > 0)
+        {
+            cmd.SetGlobalVectorArray(dirLightColorId, dirLightColors);
+            cmd.SetGlobalVectorArray(dirLightDirId, dirLightDirs);
+            cmd.SetGlobalFloat(dirLightCountId, dirLightCount);
+        }
+    }
+
+    private void SetAdditionalLightToGPU()
+    {
+        if (additonalLightCount > 0)
+        {
+            cmd.SetGlobalFloat(additionalLightCountId, additonalLightCount);
+            cmd.SetGlobalVectorArray(additionalLightColorsId, additionalLightColors);
+            cmd.SetGlobalVectorArray(additionalLightPosId, additionalPositions);
+        }
     }
     
 }
